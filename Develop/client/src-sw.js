9c2b12e0ef1b10715a6198b1936bd6137da10db4
LarryByrd89 <laryn.n99@gmail.com>
2023-08-31 18:24:43 -0400

    cashe edit

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -28,8 +28,14 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  /\.(js|css|txt|docx|pdf|html|png|jpe?g|gif|json)$/,
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
     cacheName: 'asset-cache', 
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200], 
+      }),
+    ],
   })
 );
+
